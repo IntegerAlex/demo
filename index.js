f624d6c7f85782622009512e6d1aa88154049f9b
akshat k <akshatkot@gmail.com>
2024-07-31 20:30:06 +0530

    I am dumb

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-	
+	console.log(req.header);
 	res.send('Hello Serverless');
-
+	
 })
 
-app.listen(3000, () => {
+app.listen(8080, () => {
 	console.log('Server is running on port 3000');
 }
 )
